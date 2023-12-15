5d276cb928f89ae4cb93df1e3bbb639c507d8219
regebhart <49378419+regebhart@users.noreply.github.com>
2023-12-14 17:47:02 -0700

    initial2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,8 +24,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3d182c7ef29ce78e5546801a5f7e1941",
-"/": "3d182c7ef29ce78e5546801a5f7e1941",
+"index.html": "ec98c3a2b770065c48244d01b70b0d82",
+"/": "ec98c3a2b770065c48244d01b70b0d82",
 "main.dart.js": "e2bde26e47c066732e9c1ee357c08844",
 "manifest.json": "ee28844c3b80cfa3497c893a3fdd6a32",
 "version.json": "c62c286aea42aa4bb51be6fee8a7418b"};
